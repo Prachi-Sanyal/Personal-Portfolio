d955db466076a44585f4fb0878d7e18b10794682
Prachi Sanyal <sanyalprachi1004@gmail.com>
2026-06-07 15:03:39 +0530

    frontend folder

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -2,17 +2,17 @@ import axios from 'axios';
 
 const BASE_URL =
   process.env.REACT_APP_API_URL;
-const API = axios.create({   baseURL: `${BASE_URL}/projects`
+const API = axios.create({   baseURL: `${BASE_URL}/api/projects`
 , withCredentials: true });
 const AUTH = axios.create({   baseURL: `${BASE_URL}/admin`
 , withCredentials: true });
 const CERT_API = axios.create({
-    baseURL: `${BASE_URL}/certifications`,
+    baseURL: `${BASE_URL}/api/certifications`,
   withCredentials: true,
 });
 
 const EXP_API = axios.create({
-    baseURL: `${BASE_URL}/experience`
+    baseURL: `${BASE_URL}/api/experience`
 ,
   withCredentials: true,
 });
